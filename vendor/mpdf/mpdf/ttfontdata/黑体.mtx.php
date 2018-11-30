35d01d7883432422d747cdba18cfb46c4bdf35a7
邓小明 <dxm>
2018-11-30 16:19:55 +0800

    mpdf

---
+++ vendor/mpdf/mpdf/ttfontdata/黑体.mtx.php
@@ -0,0 +1,38 @@
+<?php
+$name='SimHei';
+$type='TTF';
+$desc=array (
+  'CapHeight' => 688.0,
+  'XHeight' => 457.0,
+  'FontBBox' => '[-12 -156 996 859]',
+  'Flags' => 4,
+  'Ascent' => 859.0,
+  'Descent' => -141.0,
+  'Leading' => 0.0,
+  'ItalicAngle' => 0.0,
+  'StemV' => 87.0,
+  'MissingWidth' => 1000.0,
+);
+$unitsPerEm=256;
+$up=-86;
+$ut=47;
+$strp=254;
+$strs=47;
+$ttffile='D:/www/orkogood/vendor/mpdf/mpdf/ttfonts/simhei.ttf';
+$TTCfontID='0';
+$originalsize=9753284;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='黑体';
+$panose=' 0 0 2 1 6 9 6 1 1 1 1 1';
+$haskerninfo=false;
+$haskernGPOS=false;
+$hassmallcapsGSUB=false;
+$fontmetrics='win';
+// TypoAscender/TypoDescender/TypoLineGap = 859, -141, 141
+// usWinAscent/usWinDescent = 859, -141
+// hhea Ascent/Descent/LineGap = 859, -141, 141
+$useOTL=0x0000;
+$rtlPUAstr='';
+?>
